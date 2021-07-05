6ec392f121b613e575718314fb4d3f1129289555
Pankaj_Godhani <pankajgodhani99@gmail.com>
2021-07-05 11:34:27 +0530

    audio app

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,6 @@
 import { Component } from '@angular/core';
+import { Observable } from 'rxjs';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-root',
@@ -6,5 +8,110 @@ import { Component } from '@angular/core';
   styleUrls: ['./app.component.scss']
 })
 export class AppComponent {
-  title = 'audioapp';
+
+  audioObj = new Audio()
+  audioEvents =[
+    'ended',
+    'error',
+    'play',
+    'playing',
+    'pause',
+    'timeupdate',
+    'canplay',
+    'loadedmetadata',
+    'loadstart'
+  ]
+  files = [
+    {
+      url:'./assets/song1.mp3',
+      name:'My Song1'
+    },
+    {
+      url:'./assets/song2.mp3',
+      name:'My Song2'
+    },
+    {
+      url:'./assets/song3.mp3',
+      name:'My Song3'
+    }
+  ]
+
+  currentTime = '00:00:00'
+  duration='00:00:00'
+  seek=0
+
+  streamObserver(url:any){
+    return new Observable(observer=>{
+        this.audioObj.src = url
+        this.audioObj.load()
+        this.audioObj.play()
+
+      const handler = (event:Event) =>{
+        console.log(event);
+        this.seek = this.audioObj.currentTime;
+        this.duration = this.timeFormat(this.audioObj.duration);
+        this.currentTime = this.timeFormat(this.audioObj.currentTime);
+        
+      }
+
+      this.addEvent(this.audioObj,this.audioEvents,handler)
+
+      return ()=>{
+        this.audioObj.pause();
+        this.audioObj.currentTime = 0
+
+        this.removeEvent(this.audioObj,this.audioEvents,handler)
+      }
+    })
+  }
+
+  addEvent(obj:any,events:any,handler:any){
+    events.forEach((event:any) => {
+      obj.addEventListener(event,handler)
+    });
+  }
+
+  removeEvent(obj:any,events:any,handler:any){
+    events.forEach((event:any) => {
+      obj.removeEventListener(event,handler)
+    });
+  }
+
+  setValue(event:any){
+    this.audioObj.volume = event.target.value
+    console.log(event.target.value)
+  }
+
+  onClick(url:any){
+    this.streamObserver(url).subscribe(event=>{})
+    console.log(url);
+  }
+
+  setSeekTo(event:any){
+    this.audioObj.currentTime = event.target.value
+    console.log(event.target.value)
+  }
+
+
+  play(){
+    this.audioObj.play()
+    console.log('play button');
+  }
+
+  pause(){
+    this.audioObj.pause()
+    console.log('pause button');
+  }
+
+  stop(){
+    this.audioObj.pause()
+    this.audioObj.currentTime=0
+    console.log('stop button')
+  }
+
+  timeFormat(time:any,format="HH:mm:ss"){
+    const momentTime = time * 1000;
+    return moment.utc(momentTime).format(format)
+  }
+
 }
